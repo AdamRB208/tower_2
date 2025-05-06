9898b653472b6e31e58aa4852724b0b5122ecb63
AdamRB208 <adamrborup@gmail.com>
2025-05-06 09:36:54 -0600

    set up .env and env.js files

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-d3qj26bqdtixpiu0.us.auth0.com'
+export const clientId = 'rX26uYnOTmtdPnaKkWDK8b8KiA1HbTLi'
+export const audience = 'https://auth.optamasticprimefoundation.com'
