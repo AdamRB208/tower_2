540bcd8ceea07d8586b03c9eae4fa7caf553f702
AdamRB208 <adamrborup@gmail.com>
2025-05-06 10:23:23 -0600

    created tower events controller

---
+++ server/src/controllers/TowerEventsController.js
@@ -0,0 +1,28 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController.js"
+
+export class TowerEventsController extends BaseController {
+  constructor() {
+    super('api/events')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+
+  }
+
+  /**
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+   */
+  async createTowerEvent(request, response, next) {
+    try {
+      const towerEventData = request.body
+      const userInfo = request.userInfo
+      towerEventData.creatorId = userInfo.id
+      const towerEvent = await towerEventService.createTowerEvent(towerEventData)
+    }
+    catch (error) {
+      next(error);
+    }
+  }
+}
