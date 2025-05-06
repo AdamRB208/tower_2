595c7a7288c87d3db653e097b794ea2c5da4d355
AdamRB208 <adamrborup@gmail.com>
2025-05-06 15:32:42 -0600

    styled tower event page

--- client/src/pages/TowerEventPage.vue
+++ client/src/pages/TowerEventPage.vue
@@ -36,7 +36,77 @@ async function getTowerEventById() {
 
 
 <template>
-  <div v-if="towerEvent">{{ towerEvent }}</div>
+  <section v-if="towerEvent" class="container">
+    <div class="row">
+      <div class="col-12">
+        <img :src="towerEvent.coverImg" alt="picture of event" class="mt-5">
+        <div class="d-flex gap-5 align-items-center">
+          <h1>{{ towerEvent.name }}</h1>
+          <span class="border">{{ towerEvent.type }}</span>
+        </div>
+        <div>
+          <p class="mt-4">{{ towerEvent.description }}</p>
+          <p v-if="towerEvent.isCanceled" class="text-center">
+            <span class="mdi mdi-alert text-danger"></span>
+            This event has been canceled!
+            <span class="mdi mdi-alert text-danger"></span>
+          </p>
+        </div>
+        <div>
+          <p class="fs-4 mt-5">Date and Time</p>
+          <p class="mdi mdi-clipboard-text-clock-outline">{{ towerEvent.startDate }}</p>
+        </div>
+        <div>
+          <p class="fs-4">Location</p>
+          <p class="mdi mdi-map-marker-outline">{{ towerEvent.location }}</p>
+        </div>
+        <div>
+          <button v-if="towerEvent.creatorId == account?.id" class="btn btn-danger rounded-pill text-light">
+            {{ towerEvent.isCanceled ? 'Un-cancel' : 'Cancel Event' }} <span class="mdi"
+              :class="towerEvent.isCanceled ? 'mdi-publish' : 'mdi-close-circle'"> </span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="d-flex mb-3">
+          <div class="bg-dark-glass rounded p-2 flex-grow-1">
+            <span class="d-block"> </span>
+            <span>Tickets left : {{ towerEvent.capacity - towerEvent.ticketCount }}</span>
+          </div>
+          <!-- TODO dont show this if the event is cancelled or sold out -->
+          <button v-if="account" class="btn btn-success">
+            <span class="mdi mdi-account-plus d-block"></span>
+            <span>Attend</span>
+          </button>
+        </div>
+        <div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div
+          :title="towerEvent.ticketCount + (towerEvent.ticketCount == 1 ? ' person is ' : ' people are ') + 'attending this event'">
+          <span>Number of attendees: {{ towerEvent.ticketCount }}</span>
+        </div>
+        <!-- <ul>
+          <li class="bg-dark text-white">These people are going:</li>
+          <li v-for="ticket in tickets" :key="ticket.id">
+
+            {{ ticket.profile.name }}
+            <img :src="account.name.picture" alt="users profile image">
+          </li>
+        </ul> -->
+      </div>
+      <div class="col-md-9">
+        <!-- TODO -->
+        <!-- COMMENTS GO HERE -->
+        <!-- {{ comment }} -->
+      </div>
+    </div>
+  </section>
 </template>
 
 
