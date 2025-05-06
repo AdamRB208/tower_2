9ad2160e4c0b35a0c6f97b7e910235a9f3b6cc02
AdamRB208 <adamrborup@gmail.com>
2025-05-06 14:17:51 -0600

    rendering tower event pojos

--- client/src/services/TowerEventsService.js
+++ client/src/services/TowerEventsService.js
@@ -1,11 +1,16 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { TowerEvent } from "@/models/TowerEvent.js"
+import { AppState } from "@/AppState.js"
 
 class TowerEventsService {
 
   async getTowerEvents() {
     const response = await api.get('api/events')
     logger.log('Got Tower Events!', response.data)
+    const towerEvent = response.data.map(pojo => new TowerEvent(pojo))
+    AppState.towerEvent = towerEvent
+    logger.log('Rendering Tower Event pojos!')
   }
 
 
