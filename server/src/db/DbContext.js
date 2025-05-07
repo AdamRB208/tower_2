b045bd8e2b9d174fbacdc9128cbf2e00844192de
AdamRB208 <adamrborup@gmail.com>
2025-05-07 09:44:41 -0600

    created ticket schema and added to db context

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
 import { TowerEventsSchema } from '../models/TowerEvents.js';
+import { TicketSchema } from '../models/Ticket.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   TowerEvents = mongoose.model('TowerEvent', TowerEventsSchema);
+  Ticket = mongoose.model('Ticket', TicketSchema);
+
 }
 
 export const dbContext = new DbContext()
