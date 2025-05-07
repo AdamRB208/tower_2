610d18934db70f60356b434ccd2630615154bf4d
AdamRB208 <adamrborup@gmail.com>
2025-05-07 09:47:49 -0600

    created tickets controller

---
+++ server/src/controllers/TicketController.js
@@ -0,0 +1,12 @@
+import BaseController from "../utils/BaseController.js";
+
+export class TicketController extends BaseController {
+  constructor() {
+    super('api/tickets')
+    this.router
+
+  }
+
+
+
+}
