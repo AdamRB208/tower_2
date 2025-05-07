8abf15d7e17e78e0ef693c60b07ff598db0876b7
AdamRB208 <adamrborup@gmail.com>
2025-05-07 14:30:13 -0600

    included ticket count

--- server/src/services/TowerEventsService.js
+++ server/src/services/TowerEventsService.js
@@ -4,18 +4,18 @@ import { BadRequest, Forbidden } from "../utils/Errors.js"
 class TowerEventsService {
 
   async getAllTowerEvents() {
-    const towerEvent = await dbContext.TowerEvents.find().populate('creator', 'name picture')
+    const towerEvent = await dbContext.TowerEvents.find().populate('creator ticketCount', 'name picture')
     return towerEvent
   }
 
   async getTowerEventById(towerEventId) {
-    const towerEvent = await dbContext.TowerEvents.findById(towerEventId).populate('creator', 'name picture')
+    const towerEvent = await dbContext.TowerEvents.findById(towerEventId).populate('creator ticketCount', 'name picture')
     return towerEvent
   }
 
   async createTowerEvent(towerEventData) {
     const towerEvent = await dbContext.TowerEvents.create(towerEventData)
-    await towerEvent.populate('creator', 'name picture')
+    await towerEvent.populate('creator ticketCount', 'name picture')
     return towerEvent
   }
 
