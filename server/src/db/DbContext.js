9bf55fdb0e842abfa3816d4404a9914d93c3ea14
AdamRB208 <adamrborup@gmail.com>
2025-05-12 10:35:08 -0600

    added comments to db context

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -3,12 +3,14 @@ import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
 import { TowerEventsSchema } from '../models/TowerEvents.js';
 import { TicketSchema } from '../models/Tickets.js';
+import { CommentsSchema } from '../models/Comments.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   TowerEvents = mongoose.model('TowerEvent', TowerEventsSchema);
   Tickets = mongoose.model('Ticket', TicketSchema);
+  Comments = mongoose.model('Comment', CommentsSchema);
 
 }
 
